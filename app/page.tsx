8edbc38e479856ca4f7d675edb0071d85cf2861a
prateekvarma <prtk.varma@gmail.com>
2023-08-10 16:34:14 -0600

    Added Restaurant Card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
-import Link from "next/link";
 import NavBar from "./components/NavBar";
 import Header from "./components/Header";
+import RestaurantCard from "./components/RestaurantCard";
 
 export default function Home() {
   return (
@@ -9,36 +8,10 @@ export default function Home() {
       <main className="max-w-screen-2xl m-auto bg-white">
         <NavBar />
         <main>
-          {/* HEADER */}
           <Header />
-          {/* HEADER */} {/* CARDS */}
           <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            {/* CARD */}
-            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-              <Link href="/restaurant/milestones-grill">
-                <img
-                  src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-                  alt=""
-                  className="w-full h-36"
-                />
-                <div className="p-1">
-                  <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-                  <div className="flex items-start">
-                    <div className="flex mb-2">*****</div>
-                    <p className="ml-2">77 reviews</p>
-                  </div>
-                  <div className="flex text-reg font-light capitalize">
-                    <p className=" mr-3">Mexican</p>
-                    <p className="mr-3">$$$$</p>
-                    <p>Toronto</p>
-                  </div>
-                  <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-                </div>
-              </Link>
-            </div>
-            {/* CARD */}
+            <RestaurantCard />
           </div>
-          {/* CARDS */}
         </main>
       </main>
     </main>
