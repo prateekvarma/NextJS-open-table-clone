25e1da514f32add5ca67b30bec77658e34dba8d8
prateekvarma <prtk.varma@gmail.com>
2023-08-15 12:25:47 -0600

    Applied nested layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -10,19 +10,16 @@ import ReservationCard from "./components/ReservationCard";
 export default function RestaurantDetails() {
   return (
     <>
-      <Header />
-      <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-        <div className="bg-white w-[70%] rounded p-3 shadow">
-          <RestaurantNavbar />
-          <Title />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className="w-[27%] relative text-reg">
-          <ReservationCard />
-        </div>
+      <div className="bg-white w-[70%] rounded p-3 shadow">
+        <RestaurantNavbar />
+        <Title />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className="w-[27%] relative text-reg">
+        <ReservationCard />
       </div>
     </>
   );
