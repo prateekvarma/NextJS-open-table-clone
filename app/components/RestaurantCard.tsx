7dcfc34268494719889405dc0637caaa740daaf9
prateekvarma <prtk.varma@gmail.com>
2023-08-16 11:51:39 -0600

    Handled types for restaurant data recieved

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -1,6 +1,11 @@
 import Link from "next/link";
+import { RestaurantCardType } from "../page";
 
-export default function RestaurantCard() {
+interface Props {
+  restautant: RestaurantCardType
+}
+
+export default function RestaurantCard({}: Props) {
   return (
     <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
       <Link href="/restaurant/milestones-grill">
